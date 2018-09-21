7f714046c451e19da0a796b9da52d9ed5fd30d62
Iryna <irynamazura22@gmail.com>
2018-09-21 15:51:54 +0300

    additional_5 done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    var checkedString = str.split('');
+    var oneCoupleStorage = [];
+
+    for ( var i = 0; i < str.length; i++) {
+        for ( var j = 0; j < bracketsConfig.length; j++) {
+
+            if (checkedString[i] === bracketsConfig[j][1] && oneCoupleStorage[oneCoupleStorage.length - 1] === bracketsConfig[j][0]) {
+                oneCoupleStorage.pop();
+            }
+
+            else if (checkedString[i] === bracketsConfig[j][0]) {
+                oneCoupleStorage.push(checkedString[i]);
+            }
+        }
+    }
+
+    if (oneCoupleStorage.length === 0) return true;
+
+    else return false;
+
+};
